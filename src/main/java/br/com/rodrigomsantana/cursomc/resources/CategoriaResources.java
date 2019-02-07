8298776a3b9353bb8bb408ce697f2e7d74584db0
rodrigo santana <rodrigoms1718@gmail.com>
2019-02-06 23:38:28 -0200

    classe de dominio de categoria

--- src/main/java/br/com/rodrigomsantana/cursomc/resources/CategoriaResources.java
+++ src/main/java/br/com/rodrigomsantana/cursomc/resources/CategoriaResources.java
@@ -1,15 +1,28 @@
 package br.com.rodrigomsantana.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.rodrigomsantana.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResources {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(1, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
