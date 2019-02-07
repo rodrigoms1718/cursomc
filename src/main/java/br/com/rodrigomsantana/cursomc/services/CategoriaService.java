5195d1479148a96f3c2e5c99b6e38c7c22b3c85d
rodrigo santana <rodrigoms1718@gmail.com>
2019-02-07 00:29:31 -0200

    criando repository e service para Categoria

---
+++ src/main/java/br/com/rodrigomsantana/cursomc/services/CategoriaService.java
@@ -0,0 +1,24 @@
+package br.com.rodrigomsantana.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.rodrigomsantana.cursomc.domain.Categoria;
+import br.com.rodrigomsantana.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		
+		Optional<Categoria> obj = repo.findById(id);
+		
+		return obj.orElse(null);
+	}
+
+}
