48bdce1d576ba6f6d8b830d07e699c507ac21879
rodrigo santana <rodrigoms1718@gmail.com>
2019-02-08 23:41:58 -0200

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/br/com/rodrigomsantana/cursomc/services/PedidoService.java
@@ -0,0 +1,27 @@
+package br.com.rodrigomsantana.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.rodrigomsantana.cursomc.domain.Pedido;
+import br.com.rodrigomsantana.cursomc.repositories.PedidoRepository;
+import br.com.rodrigomsantana.cursomc.services.exceptions.ObjectNotFoundException;
+
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido find(Integer id) {
+		
+		Optional<Pedido> obj = repo.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+
+}
